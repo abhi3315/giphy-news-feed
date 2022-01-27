d0013e9e1405f3c18a9153915a60688e30030745
abhi3315 <abhishek.sharma@rtcamp.com>
2022-01-28 00:28:19 +0530

    feat: search and load more gifs

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,29 +1,88 @@
 import React from "react";
-import { Radio, Input, Row, Col, Button } from "antd";
+import { Radio, Input, Row, Col, Button, Spin, message } from "antd";
+import { getGifs } from "../utils/giphy";
+import { debouncer } from "../utils/debouncer";
 
-const Giphy = ({ gifs }) => {
+const Giphy = () => {
+	const [gifs, setGifs] = React.useState([]);
+	const [loading, setLoading] = React.useState(false);
+	const [search, setSearch] = React.useState("");
+	const [offset, setOffset] = React.useState(0);
+
+	const loaderRef = React.useRef();
+
+	React.useEffect(() => {
+		(async () => {
+			setLoading(true);
+			try {
+				const limit = 10;
+				const { data } = await getGifs({ offset, limit, search });
+				setGifs([...gifs, ...data]);
+			} catch (e) {
+				message.error({ content: "Unable to load Gifs" });
+			}
+			setLoading(false);
+		})();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [offset, search]);
+
+	const handleObserver = React.useCallback((entries) => {
+		const target = entries[0];
+		if (target.isIntersecting && !loading) {
+			setOffset((prev) => prev + 10);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	React.useEffect(() => {
+		const option = {
+			root: null,
+			rootMargin: "20px",
+			threshold: 0,
+		};
+		const observer = new IntersectionObserver(handleObserver, option);
+		if (loaderRef.current) observer.observe(loaderRef.current);
+	}, [handleObserver]);
+
+	const handleSearch = (e) => {
+		const { value } = e.target;
+		setSearch(value);
+		setGifs([]);
+	};
+
 	return (
 		<>
 			<Radio.Group optionType="button" buttonStyle="solid">
-				<Row gutter={16} className="gif-container mb-10">
-					{gifs.map(({ id }) => (
-						<Col key={id} className="gutter-row" span={12}>
-							<Radio.Button value={id} className="mb-10 img-label">
-								<img
-									src={`https://i.giphy.com/media/${id}/100.webp`}
-									alt="gif"
-									height="100"
-									width="100"
-								/>
-							</Radio.Button>
-						</Col>
-					))}
-				</Row>
-				<Row gutter={16} className="w-300px">
-					<Col span={18}>
-						<Input placeholder="Search Gifs" />
+				<div className="gif-container mb-10">
+					<Row gutter={16} className="w-300px m-auto">
+						{gifs.map(({ id }) => (
+							<Col key={id} className="gutter-row" span={12}>
+								<Radio.Button value={id} className="mb-10 img-label">
+									<img
+										src={`https://i.giphy.com/media/${id}/100.webp`}
+										alt="gif"
+										height="100"
+										width="100"
+									/>
+								</Radio.Button>
+							</Col>
+						))}
+						<div ref={loaderRef} />
+						{!loading && (
+							<div className="w-100">
+								<Spin className="m-auto d-block" />
+							</div>
+						)}
+					</Row>
+				</div>
+				<Row gutter={16} className="w-300px m-auto">
+					<Col span={16}>
+						<Input
+							placeholder="Search Gifs"
+							onInput={debouncer(handleSearch, 1000)}
+						/>
 					</Col>
-					<Col span={6}>
+					<Col span={8}>
 						<Button className="width100">Select GIF</Button>
 					</Col>
 				</Row>
--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,28 +1,9 @@
 import React from "react";
 import { Modal, Input, Popover, Button } from "antd";
 import Giphy from "./Giphy";
-import { getGifs } from "../utils/giphy";
 
 const NewPost = ({ toggleModel, isOpen }) => {
-	const [gifs, setGifs] = React.useState([]);
-	const [error, setError] = React.useState(false);
-	const [loading, setLoading] = React.useState(false);
-	const [loadingMore, setLoadingMore] = React.useState(false);
-	const [search, setSearch] = React.useState("");
-	const [offset, setOffset] = React.useState(0);
-
 	const { TextArea } = Input;
-
-	React.useEffect(() => {
-		(async () => {
-			try {
-				const limit = 10;
-				const { data } = await getGifs({ offset, limit });
-				setGifs([...gifs, ...data]);
-			} catch (e) {}
-		})();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [offset]);
 
 	return (
 		<Modal
@@ -37,7 +18,7 @@ const NewPost = ({ toggleModel, isOpen }) => {
 					title="Select Gif"
 					trigger="click"
 					key="gif"
-					content={<Giphy gifs={gifs} />}
+					content={<Giphy />}
 				>
 					<Button>Select Gif</Button>
 				</Popover>,
